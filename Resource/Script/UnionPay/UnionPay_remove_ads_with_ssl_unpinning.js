2c8263fcbce84667a51ef8b22bc72e465c36b94c
palapal <palapal708@cumzle.com>
2023-11-29 21:50:44 +0800

    更新银联云闪付

--- Resource/Script/UnionPay/UnionPay_remove_ads_with_ssl_unpinning.js
+++ Resource/Script/UnionPay/UnionPay_remove_ads_with_ssl_unpinning.js
@@ -1,7 +1,7 @@
 /*
 脚本引用https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/breakssl.js
 */
-// 2023-11-24 19:40
+// 2023-11-29 16:30
 
 const url = $request.url;
 const header = $request.headers;
@@ -9,10 +9,10 @@ const ua = header["User-Agent"] || header["user-agent"];
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (ua.includes("iPhone CHSP")) {
+if (ua === "iPhone CHSP") {
   // 云闪付
-  if (url.includes("/app/inApp/sys/appShowGroup")) {
-    // 云闪付首页
+  if (url.includes("/app/inApp/sys/appShowGroup/")) {
+    // 各项功能列表
     if (obj?.params?.groups?.length > 0) {
       let newGroups = [];
       for (let group of obj.params.groups) {
@@ -25,6 +25,7 @@ if (ua.includes("iPhone CHSP")) {
                 "8021", // 首页顶栏-生活便利区
                 "8022", // 快捷功能栏
                 "8202", // 金融页-顶栏图标
+                "8702", // 卡管理-功能列表
                 "8800", // 首页顶栏-搜索按钮
                 "9003", // 首页信息流-本地专区
                 "9302", // 我的页面-右上角功能区
@@ -51,15 +52,14 @@ if (ua.includes("iPhone CHSP")) {
                       "2117", // 我的页面-积分
                       "23445", // 快捷功能栏-我的奖励
                       "23446", // 我的页面-合作共赢
-                      "23451", // 我的页面-精选列表
-                      "23451", // 我的页面-精选服务
+                      "23451", // 我的页面-精选列表&精选列表
+                      "280878", // 卡管理-云惠保
                       "281988", // 首页顶栏-商城
                       "282180", // 金融页-跨境理财通
                       "284486", // 我的页面-商家服务
                       "285323", // 金融页-大额分期
                       "287201", // 金融页-小微贷
-                      "289722", // 快捷功能栏-小程序榜单
-                      "289722", // 我的页面-小程序榜单
+                      "289722", // 快捷功能栏&我的页面-小程序榜单
                       "43121", // 金融页-体验金
                       "62225", // 首页顶栏-充值中心
                       "62249", // 首页顶栏-借款
@@ -73,11 +73,12 @@ if (ua.includes("iPhone CHSP")) {
                       "62997", // 金融页-申请信用卡
                       "62999", // 金融页-基金
                       "758", // 首页顶栏-申请信用卡
+                      "8016", // 卡管理-黄金
                       "8027", // 金融页-安全保障
                       "82023", // 金融页-返现主题卡
                       "82025", // 金融页-黄金
                       "8203", // 金融页-财富信贷
-                      "85188", // 金融页-云闪付卡
+                      "85188", // 金融页-云闪付主题卡
                       "911", // 首页顶栏-银联无界卡
                       "9888", // 首页信息流-出行服务
                       "9994" // 首页顶栏-计划礼券
@@ -100,6 +101,11 @@ if (ua.includes("iPhone CHSP")) {
         newGroups.push(group);
       }
       obj.params.groups = newGroups;
+    }
+  } else if (url.includes("/life/inApp/wealth/home/")) {
+    // 财富页
+    if (obj?.params) {
+      obj.params = {};
     }
   }
 }
