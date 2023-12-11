062cb4f9f401f14fa07e54264ec44b22c0103d93
palapal <palapal708@cumzle.com>
2023-12-11 20:58:20 +0800

    更新

--- Resource/Script/IThome/IThome_remove_ads.js
+++ Resource/Script/IThome/IThome_remove_ads.js
@@ -2,13 +2,16 @@
 引用脚本https://raw.githubusercontent.com/toulanboy/scripts/master/ithome_ad/ithome_ad.js
 脚本二改https://github.com/Keywos/rule/raw/main/JS/ithomes.js
 */
-let lbt = true, zd = true, lbtAd = true;
+const isLoon = typeof $loon !== "undefined";
+let lbt = true,
+  zd = true,
+  lbtAd = true;
 if (isLoon) {
-    zd = $persistentStore.read("移除置顶项") === "开启";
-    lbtAd = $persistentStore.read("移除轮播图广告") === "开启";
-    lbt = $persistentStore.read("移除全部轮播图") === "开启";
+  zd = $persistentStore.read("移除置顶项") === "开启";
+  lbtAd = $persistentStore.read("移除轮播图广告") === "开启";
+  lbt = $persistentStore.read("移除全部轮播图") === "开启";
 }
-let FeedTypes = [10000];
+let FeedTypes = [];
 
 let i = JSON.parse($response.body);
 if (i?.data?.list) {
@@ -28,11 +31,9 @@ if (i?.data?.list) {
   zd && FeedTypes.push(10003); //置顶
   i.data.list = i.data.list.filter((item) => {
     return (
-      !FeedTypes.includes(item.feedType) ||
-      item.feedContent.smallTags[0]?.text === null ||
-      !item.feedContent.smallTags[0].text.includes("广告")
+      !FeedTypes.includes(item.feedType) &&
+      !item.feedContent.smallTags?.[0].text.includes("广告")
     );
   });
 }
-
 $done({ body: JSON.stringify(i) });
