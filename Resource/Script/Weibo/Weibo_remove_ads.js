2c297b653f2be6381b6c5a253f4d7390ad3c207d
palapal <palapal708@cumzle.com>
2024-01-18 16:19:48 +0800

    更新

--- Resource/Script/Weibo/Weibo_remove_ads.js
+++ Resource/Script/Weibo/Weibo_remove_ads.js
@@ -1,7 +1,7 @@
 /*
 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/weibo.js
 */
-// 2024-01-11 15:50
+// 2024-01-18 16:05
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -937,6 +937,8 @@ if (url.includes("/interface/sdk/sdkad.php")) {
                   // 放行无itemid字段的内容
                   newII.push(ii);
                 }
+                // 头像挂件,关注按钮
+                removeAvatar(ii?.data);
                 item.items = newII;
               }
             } else {
