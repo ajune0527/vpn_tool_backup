77ea7d0a557e38ff17e5d3961d28ea334682e0fe
palapal <palapal708@cumzle.com>
2024-01-23 16:03:03 +0800

    新增Soul去广告

---
+++ Resource/Script/Soul/Soul_remove_ads.js
@@ -0,0 +1,34 @@
+/*
+脚本引用https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Soul.js
+*/
+//2024-01-22 22:22  感谢@可莉对去除开屏广告提供的帮助
+const url = $request.url;
+const scriptEnvironment = typeof $task != 'undefined' ? 'Surge' : (typeof $loon != 'undefined' ? 'Loon' : (typeof $httpClient != 'undefined' ? 'Qx' : 'Unknown'));
+if (!$response.body || scriptEnvironment === 'Unknown') {
+  $done({});
+}
+let obj = JSON.parse($response.body);
+
+const shouldDeleteData = (url) => {
+  return url.includes("/post/homepage/guide/card") ||
+         url.includes("/furion/position/content") ||
+         url.includes("/hot/soul/rank") ||
+         url.includes("/post/gift/list") ||
+         url.includes("/mobile/app/version/queryIos") ||
+         url.includes("/teenager/config") ||
+        //  url.includes("/winterfell/v2/getIpByDomain");
+};
+
+const shouldModifyLimitInfo = (url, obj) => {
+  return url.includes("/chat/limitInfo") && obj.data && obj.data.limit !== undefined;
+};
+
+if (!obj.data || shouldDeleteData(url)) {
+  delete obj.data;
+}
+
+if (shouldModifyLimitInfo(url, obj)) {
+  obj.data.limit = false;
+}
+
+$done({ body: JSON.stringify(obj) });
