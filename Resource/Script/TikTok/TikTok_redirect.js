f817f9da2283d26c26313cd35cd77c61b6618a78
palapal <palapal708@cumzle.com>
2024-04-03 13:20:17 +0800

    更新

--- Resource/Script/TikTok/TikTok_redirect.js
+++ Resource/Script/TikTok/TikTok_redirect.js
@@ -17,8 +17,8 @@ if (/(tnc|dm).+\.[^\/]+\.com\/\w+\/v\d\/\?/.test(url)) {
   $done({ response });
 }
 // URL请求重定向替换
-else if (/_region\d?=CN&|&mcc_mnc=4/.test(url)) {
-  url = url.replace(/_region\d?=CN&/g, `_region=${loc}&`).replace(/&mcc_mnc=\d+/g, "&mcc_mnc=44023");
+else if (/_region=CN&|_region1=CN&|&mcc_mnc=\d+/.test(url)) {
+  url = url.replace(/_region=CN&/g, `_region=${loc}&`).replace(/_region1=CN&/g, `_region1=${loc}&`).replace(/&mcc_mnc=\d+/g, "&mcc_mnc=44023");
   console.log(url)
   const response = {
     status: 307,
