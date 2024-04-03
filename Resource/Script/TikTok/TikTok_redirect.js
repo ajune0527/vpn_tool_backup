5c72866e9dfb9916f17f0ce3a2c031fa4cf2a4d7
palapal <palapal708@cumzle.com>
2024-04-03 11:16:51 +0800

    更新

--- Resource/Script/TikTok/TikTok_redirect.js
+++ Resource/Script/TikTok/TikTok_redirect.js
@@ -7,15 +7,15 @@ lk = $persistentStore.read("解锁地区"),loc = keyus[lk] || "KR",url = $reques
 //   inkeys = $persistentStore.read("手动输入地区代码[可选]");
 //   loc = inkeys
 // }
-if (/(tnc|dm).+\.[^\/]+\.com\/\w+\/v\d\/\?/.test(url)) {
+if (/https?:\/\/.+.(tiktokv|tiktokcdn).com\/.+(region\d?=CN|&mcc_mnc=\d+)/.test(url)) {
   url = url.replace(/\/\?/g,'');
   const response = {
     status: 302,
     headers: {Location: url},
   };
   $done({response});
-} else if (/_region=CN&|&mcc_mnc=4/.test(url)) {
-  url = url.replace(/_region=CN&/g,`_region=${loc}&`).replace(/&mcc_mnc=4/g,"&mcc_mnc=2");
+} else if (/region\d?=CN&|&mcc_mnc=\d+/.test(url)) {
+  url = url.replace(/region\d?=CN&/g,`_region=${loc}&`).replace(/&mcc_mnc=\d+/g,"&mcc_mnc=44023");
   const response = {
     status: 307,
     headers: {Location: url},
